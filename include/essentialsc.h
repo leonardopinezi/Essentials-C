58c0fd0125aeaa9d509e003bd1584a3ce4c2e236
trindadedev13 <trindadedev13@gmail.com>
2025-05-27 23:37:52 -0300

    feat(Essentials-C): Use More C-Like functions names, format code
    use snake case in functions instead camelCase, use clangd-format with WebKit Style to format files

--- include/essentialsc.h
+++ include/essentialsc.h
@@ -1,22 +1,42 @@
 #ifndef ESSENTIALS_H
 #define ESSENTIALS_H
 
+/** Old Names to avoid broke existing projects. */
+#define replaceChar replace_char
+#define bubbleSort bubble_sort
+#define startsWith starts_with
+#define endsWith ends_with
+
 typedef char str[]; // Experimental
 
 int length(char s[]);
+
 void input(char s[], char output[]);
+
 int indexof(char s[], char letter);
+
 int lastindexof(char s[], char letter);
+
 void reverse(char s[], char output[]);
+
 void substring(char s[], char output[], int start, int end);
+
 void trim(char s[], char output[]);
-void replaceChar(char s[], char letter, char rletter);
+
+void replace_char(char s[], char letter, char rletter);
+
 int includes(char s[], char word[]);
-void bubbleSort(int arr[], int n, int val);
-int startsWith(char entry[], char character[1]);
-int endsWith(char entry[], char character[]);
+
+void bubble_sort(int arr[], int n, int val);
+
+int starts_with(char entry[], char character[1]);
+
+int ends_with(char entry[], char character[]);
+
 void uppercase(char s[], char output[]);
+
 void lowercase(char s[], char output[]);
+
 void swapcase(char s[], char output[]);
 
 #endif
--- src/essentialsc.c
+++ src/essentialsc.c
@@ -1,254 +1,267 @@
-#include <stdio.h>
 #include "essentialsc.h"
 
+#include <stdio.h>
+
 /*
-	<< ESSENTIALS-C >>
+  << ESSENTIALS-C >>
 
-	Simple or Important essentials functions made in C
+  Simple or Important essentials functions made in C
 
-	This project is Open Source with MIT license, so you can feel free to Download and edit this project to improve it
-	- Give the credits to the author <3
+  This project is Open Source with MIT license, so you can feel free to Download
+  and edit this project to improve it
+  - Give the credits to the author <3
 
-	Check the Doc and learn how to use : https://github.com/oileo/Essentials-C
-	
-	Made by Shinpi (Leonardo Gonçalves)
-	Version : 3
+  Check the Doc and learn how to use : https://github.com/oileo/Essentials-C
 
-	Made in Brazil
+  Made by Shinpi (Leonardo Gonçalves)
+  Version : 3
+
+  Made in Brazil
 */
 
 void input(char s[], char output[])
 {
-	printf("%s", s);
-	int i = 0;
-	char c;
+  printf("%s", s);
+  int i = 0;
+  char c;
 
-	while((c = getchar()) != '\n' && c != EOF) {
-		output[i] = c;
-		++i;
-	}
+  while ((c = getchar()) != '\n' && c != EOF) {
+    output[i] = c;
+    ++i;
+  }
 
-	output[i] = '\0';
+  output[i] = '\0';
 }
 
 int length(char s[])
 {
-	int n = 0;
-	while(s[n] != '\0' && s[n] != '\n')
-		n++;
+  int n = 0;
+  while (s[n] != '\0' && s[n] != '\n')
+    n++;
 
-	return n;
+  return n;
 }
 
 int indexof(char s[], char letter)
 {
-	int l = 0;
-	while(l < (length(s) - 1)) {
-		if(s[l] == letter) {
-			return l;
-		}
-		++l;
-	}
-	return -1;
+  int l = 0;
+  while (l < (length(s) - 1)) {
+    if (s[l] == letter) {
+      return l;
+    }
+    ++l;
+  }
+  return -1;
 }
 
 int lastindexof(char s[], char letter)
 {
-	int l = length(s) - 1;
-	while(l > 0) {
-		if(s[l] == letter) {
-			return l;
-		}
-		--l;
-	}
-	return -1;
+  int l = length(s) - 1;
+  while (l > 0) {
+    if (s[l] == letter) {
+      return l;
+    }
+    --l;
+  }
+  return -1;
 }
 
 void reverse(char s[], char output[])
 {
-	int l = length(s)-1;
-	int a = 0;
+  int l = length(s) - 1;
+  int a = 0;
 
-	while(l >= 0) {
-		if(s[l] != '\0' && s[l] != '\n') output[a] = s[l];
-		++a;
-		--l;
-	}
-	--l;
-	output[a] = '\0';
+  while (l >= 0) {
+    if (s[l] != '\0' && s[l] != '\n')
+      output[a] = s[l];
+    ++a;
+    --l;
+  }
+  --l;
+  output[a] = '\0';
 }
 
 void substring(char s[], char output[], int start, int end)
 {
-	int size = length(s) - 2;
+  int size = length(s) - 2;
 
-	if(start > size || start < 0) printf("Error : the string start need be more than 0 and in size of string\n");
-	if(end > size || end < 0) printf("Error : the string end need be more than 0 and in size of string\n");
-	if(start > end) printf("Error : the substring end need be more than start\n");	
+  if (start > size || start < 0)
+    printf(
+        "Error : the string start need be more than 0 and in size of string\n");
+  if (end > size || end < 0)
+    printf(
+        "Error : the string end need be more than 0 and in size of string\n");
+  if (start > end)
+    printf("Error : the substring end need be more than start\n");
 
-	if(start > size || start < 0 || end > size || end < 0 || start > end) return;
+  if (start > size || start < 0 || end > size || end < 0 || start > end)
+    return;
 
-	int i = start;
-	int j = 0;
-	while(i < end) {
-		if(s[i] != '\0' && s[i] != '\n') {
-			output[j] = s[i];
-			++j;
-		}
-		++i;
-	}
+  int i = start;
+  int j = 0;
+  while (i < end) {
+    if (s[i] != '\0' && s[i] != '\n') {
+      output[j] = s[i];
+      ++j;
+    }
+    ++i;
+  }
 
-	output[i] = '\0';
+  output[i] = '\0';
 }
 
 void trim(char s[], char output[])
 {
-	int i, j;
+  int i, j;
 
-	i = j = 0;
-	
-	int __length = length(s);
-	int _space = 0;
+  i = j = 0;
 
-	while(i < __length){
-		if(s[i] == ' ') {
-			if(_space == 1) {
-				if(i >= 1 && s[i-1] == ' ') break;
-				output[j] = s[i];
-				j++;
-			}
-		} else {
-			if(_space == 0)
-				_space = 1;
-			output[j] = s[i];
-			j++;
-		}
-		++i;
-	}
+  int __length = length(s);
+  int _space = 0;
 
-	_space = 0;
-	if(i >= 1 && s[i-1] == ' ') {
-		output[j-1] = '\0';
-	} else output[j] = '\0';
+  while (i < __length) {
+    if (s[i] == ' ') {
+      if (_space == 1) {
+        if (i >= 1 && s[i - 1] == ' ')
+          break;
+        output[j] = s[i];
+        j++;
+      }
+    } else {
+      if (_space == 0)
+        _space = 1;
+      output[j] = s[i];
+      j++;
+    }
+    ++i;
+  }
+
+  _space = 0;
+  if (i >= 1 && s[i - 1] == ' ') {
+    output[j - 1] = '\0';
+  } else
+    output[j] = '\0';
 }
 
-void replaceChar(char s[], char letter, char rletter)
+void replace_char(char s[], char letter, char rletter)
 {
-	int _l = 0;
+  int _l = 0;
 
-	while(_l < length(s)) {
-		++_l;
-		if(s[_l] == letter) {
-			s[_l] = rletter;
-		}
-	}
+  while (_l < length(s)) {
+    ++_l;
+    if (s[_l] == letter) {
+      s[_l] = rletter;
+    }
+  }
 }
 
 int includes(char s[], char word[])
 {
-	int h = 0;
-	int count = 0;
-	int count1 = -1;
+  int h = 0;
+  int count = 0;
+  int count1 = -1;
 
-	while(count < length(s)) {
-		if(count1 > -1 && count1 < length(word)) ++count1;
-		if(s[count] == word[0]) {
-			h = 1;
-			++count1;
-		}
-
-		if(s[count] != word[count1]) {
-			if(word[count1] != '\0') {
-				h = 0;				
-			}
-		}
-
-		++count;
-	}
-	
-	return h;
-}
-
-void bubbleSort(int arr[], int n, int val)
-{
-    int i, j, temp, swapped;
-
-    for (i = 0; i < n - 1; i++) {
-        swapped = 0;
-        for (j = 0; j < n - i - 1; j++) {
-            if ((val && arr[j] > arr[j + 1]) || (!val && arr[j] < arr[j + 1])) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-                swapped = 1;
-            }
-        }
-        if (swapped == 0) break;
+  while (count < length(s)) {
+    if (count1 > -1 && count1 < length(word))
+      ++count1;
+    if (s[count] == word[0]) {
+      h = 1;
+      ++count1;
     }
+
+    if (s[count] != word[count1]) {
+      if (word[count1] != '\0') {
+        h = 0;
+      }
+    }
+
+    ++count;
+  }
+
+  return h;
 }
 
-int startsWith(char entry[], char character[])
+void bubble_sort(int arr[], int n, int val)
 {
-	if(entry[0] == character[0]) {
-		return 0;
-	} else {
-		return 1;
-	}
+  int i, j, temp, swapped;
+
+  for (i = 0; i < n - 1; i++) {
+    swapped = 0;
+    for (j = 0; j < n - i - 1; j++) {
+      if ((val && arr[j] > arr[j + 1]) || (!val && arr[j] < arr[j + 1])) {
+        temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        swapped = 1;
+      }
+    }
+    if (swapped == 0)
+      break;
+  }
 }
 
-int endsWith(char entry[], char character[])
+int starts_with(char entry[], char character[])
 {
-	if(entry[length(entry) - 1] == character[length(character)]) {
-		return 0;
-	} else {
-		return 1;
-	}
+  if (entry[0] == character[0]) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
+int ends_with(char entry[], char character[])
+{
+  if (entry[length(entry) - 1] == character[length(character)]) {
+    return 0;
+  } else {
+    return 1;
+  }
 }
 
 void uppercase(char s[], char output[])
 {
-	if (s == NULL || output == NULL) {
-		return;
-	} else {
-		int i = 0;
-		while (i < length(s)) {
-			output[i] = s[i] >= 'a' && s[i] <= 'z' ? s[i] - 'a' + 'A' : s[i];
-			i++;
-		}
-		output[i] = '\0';
-	}
+  if (s == NULL || output == NULL) {
+    return;
+  } else {
+    int i = 0;
+    while (i < length(s)) {
+      output[i] = s[i] >= 'a' && s[i] <= 'z' ? s[i] - 'a' + 'A' : s[i];
+      i++;
+    }
+    output[i] = '\0';
+  }
 }
 
 void lowercase(char s[], char output[])
 {
-	if (s == NULL || output == NULL) {
-		return;
-	} else {
-		int i = 0;
-		while (i < length(s)) {
-			output[i] = s[i] >= 'A' && s[i] <= 'Z' ? s[i] - 'A' + 'a' : s[i];
-			i++;
-		}
-		output[i] = '\0';
-	}
+  if (s == NULL || output == NULL) {
+    return;
+  } else {
+    int i = 0;
+    while (i < length(s)) {
+      output[i] = s[i] >= 'A' && s[i] <= 'Z' ? s[i] - 'A' + 'a' : s[i];
+      i++;
+    }
+    output[i] = '\0';
+  }
 }
 
-void swapcase(char s[], char output[]) 
+void swapcase(char s[], char output[])
 {
-	if (s == NULL || output == NULL) {
-		return;
-	} else {
-		int i = 0;
-		while (i < length(s)) {
-			if (s[i] >= 'A' && s[i] <= 'Z') {
-				output[i] = s[i] - 'A' + 'a';
-			} else if (s[i] >= 'a' && s[i] <= 'z') {
-				output[i] = s[i] - 'a' + 'A';
-			} else {
-				output[i] = s[i];
-			}
-			i++;
-		}
-		output[i] = '\0';
-	}
+  if (s == NULL || output == NULL) {
+    return;
+  } else {
+    int i = 0;
+    while (i < length(s)) {
+      if (s[i] >= 'A' && s[i] <= 'Z') {
+        output[i] = s[i] - 'A' + 'a';
+      } else if (s[i] >= 'a' && s[i] <= 'z') {
+        output[i] = s[i] - 'a' + 'A';
+      } else {
+        output[i] = s[i];
+      }
+      i++;
+    }
+    output[i] = '\0';
+  }
 }
